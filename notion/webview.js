a8f41786e1606e74f56aec92918a4cec3a52b48a
Jonathan Sun <Jspsun@gmail.com>
2018-10-25 13:39:53 -0700

    initial

---
+++ notion/webview.js
@@ -0,0 +1,14 @@
+// notifs
+module.exports = (Franz) => {
+  function getMessages() {
+    let direct = 0;
+    let indirect = 0;
+    const FranzData = document.querySelector('#FranzMessages').dataset;
+    if (FranzData) {
+      direct = FranzData.direct;
+      indirect = FranzData.indirect;
+    }
+    Franz.setBadge(direct, indirect);
+  }
+  Franz.loop(getMessages);
+}
